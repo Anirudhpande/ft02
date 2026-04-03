302d7e8326d2223f7e657b64b632777b9ef04e55
varad <varad@varadhapsajeev.com>
2026-04-03 19:45:22 +0530

    added .gitignore and moved database credentials to .env

--- ft02/backend/database/db.py
+++ ft02/backend/database/db.py
@@ -1,7 +1,11 @@
+import os
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://postgres.dpcmvystzvchfcxznpyb:CXIBBGxysB8Foxld@aws-1-ap-northeast-1.pooler.supabase.com:6543/postgres"
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 
